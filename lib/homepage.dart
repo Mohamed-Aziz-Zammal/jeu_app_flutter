e403d1e95bdb74fe4fa10dcf3287daf0b80bd5ac
mohamed-aziz <medazizzammal@gmail.com>
2024-06-05 22:15:07 +0100

    feat: Add animal pictures as assets

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jeu_app/flipcardgame.dart';
+import 'package:jeu_app/data.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -17,72 +19,85 @@ class _HomepageState extends State<Homepage> {
         child: ListView.builder(
           itemCount: list.length,
           itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Stack(
-                children: [
-                  Container(
-                    height: 100,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: list[index].secomdarycolor,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => list[index].goto,
+                  ),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: list[index].secomdarycolor,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
                             blurRadius: 4,
                             color: Colors.black45,
                             spreadRadius: 0.5,
-                            offset: Offset(3, 4)),
-                      ],
+                            offset: Offset(3, 4),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                  Container(
-                    height: 90,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: list[index].primarycolor,
-                      borderRadius: BorderRadius.circular(30),
-                      boxShadow: [
-                        BoxShadow(
+                    Container(
+                      height: 90,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: list[index].primarycolor,
+                        borderRadius: BorderRadius.circular(30),
+                        boxShadow: [
+                          BoxShadow(
                             blurRadius: 4,
                             color: Colors.black12,
                             spreadRadius: 0.3,
-                            offset: Offset(
-                              5,
-                              3,
-                            ))
-                      ],
-                    ),
-                    child: Column(
-                      children: [
-                        Center(
+                            offset: Offset(5, 3),
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
                             child: Text(
-                          list[index].name,
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              shadows: [
-                                Shadow(
+                              list[index].name,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                                shadows: [
+                                  Shadow(
                                     color: Colors.black26,
                                     blurRadius: 2,
-                                    offset: Offset(1, 2)),
-                                Shadow(
+                                    offset: Offset(1, 2),
+                                  ),
+                                  Shadow(
                                     color: Colors.green,
                                     blurRadius: 2,
-                                    offset: Offset(0.5, 2))
-                              ]),
-                        )),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: genratestar(list[index].noOfstar),
-                        )
-        
-                      ],
+                                    offset: Offset(0.5, 2),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: genratestar(list[index].noOfstar),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           },
@@ -90,34 +105,33 @@ class _HomepageState extends State<Homepage> {
       ),
     );
   }
+}
 
-  List<Widget> genratestar(int no) {
-    List<Widget> _icons = [];
-    for (int i = 0; i < no; i++) {
-      _icons.insert(
-          i,
-          Icon(
-            Icons.star,
-            color: Colors.yellow,
-          ));
-    }
-    return _icons;
+List<Widget> genratestar(int no) {
+  List<Widget> _icons = [];
+  for (int i = 0; i < no; i++) {
+    _icons.insert(
+        i,
+        Icon(
+          Icons.star,
+          color: Colors.yellow,
+        ));
   }
+  return _icons;
 }
 
 class Details {
   String name;
   Color primarycolor;
   Color secomdarycolor;
-  //Widget goto;
-   int noOfstar;
+  Widget goto;
+  int noOfstar;
   Details({
     required this.name,
     required this.primarycolor,
     required this.secomdarycolor,
-    //required this.goto,
+    required this.goto,
     this.noOfstar = 0,
-    
   });
 }
 
@@ -126,19 +140,21 @@ List<Details> list = [
     name: "EASY",
     primarycolor: Colors.green,
     secomdarycolor: Colors.green[200] ?? Colors.green,
-    //goto: Text("Easy"),
     noOfstar: 1,
+    goto: FlipCardGame(Level.Easy),
   ),
   Details(
     name: "MEDIUM",
     primarycolor: Colors.orange,
     secomdarycolor: Colors.orange[200] ?? Colors.orange,
     noOfstar: 2,
+    goto: FlipCardGame(Level.Medium),
   ),
   Details(
     name: "HARD",
     primarycolor: Colors.red,
     secomdarycolor: Colors.red[200] ?? Colors.red,
     noOfstar: 3,
-  )
+    goto: FlipCardGame(Level.Hard),
+  ),
 ];
